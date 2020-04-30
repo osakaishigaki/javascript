236c91c431dfb03a25ba1c2e31db20904d7a7c9c
osakaishigaki <osakaishigaki@gmail.com>
2020-04-30 21:08:50 +0900

    2108 p204

--- book-js/5-02_location/step1/script.js
+++ book-js/5-02_location/step1/script.js
@@ -1,3 +1,9 @@
-document.getElementById('form').select.onchange = function() {
+// document.getElementById('form').select.onchange = function() {
+// 	location.href = document.getElementById('form').select.value;
+// }
+
+
+
+document.getElementById('form').querySelector.onchange = function(){
 	location.href = document.getElementById('form').select.value;
 }
